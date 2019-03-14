6afd55fede438aaee484f9d481c93a08db94105c
Rishi Ezhava <rishiezhava@gmail.com>
2019-03-14 23:58:45 +0530

    Delete footer.php

--- CI/footer.php
+++
@@ -1,6 +0,0 @@
-</body>
-</html>
-<script src="<?= base_url('assets/js/jquery-3.3.1.js'); ?>"></script>
-<script src="<?=  base_url('assets/js/jquery.dataTables.min.js');?>"></script>
-<script src="<?= base_url('assets/js/dataTables.bootstrap4.min.js');?>"></script>
-<script src="<?= base_url('assets/js/custom_js.js'); ?>"></script>
