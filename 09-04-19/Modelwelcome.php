c42e85dfb6ce61b751c2aa2411d5e86e1981ac9f
Rishi Ezhava <rishiezhava@gmail.com>
2019-04-09 08:51:38 +0530

    latest

---
+++ 09-04-19/Modelwelcome.php
@@ -0,0 +1,267 @@
+<?php
+
+class Modelwelcome extends CI_Model
+{
+    public function GetAccessToken($client_id, $redirect_uri, $client_secret, $code)
+    {
+        $url = 'https://accounts.google.com/o/oauth2/token';
+        $curlPost = 'client_id=' . $client_id . '&redirect_uri=' . $redirect_uri . '&client_secret=' . $client_secret . '&code=' . $code . '&grant_type=authorization_code';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+        $data = json_decode(curl_exec($ch), true);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if ($http_code != 200)
+            throw new Exception('Error : Failed to receieve access token');
+
+//        $userId=$this->session->userId;
+        $userId=1;
+        if(isset($data['refresh_token'])){
+            $tokkenArray=array(
+                'user_id'=>$userId,
+                'access_token'=>$data['access_token'],
+                'access_expiry_time'=>date('Y-m-d H:i:s'),
+                'refresh_token'=>$data['refresh_token']
+            );
+        }
+        else{
+            $tokkenArray=array(
+                'user_id'=>$userId,
+                'access_token'=>$data['access_token'],
+                'access_expiry_time'=>date('Y-m-d H:i:s')
+            );
+        }
+
+        $userExists = $this->db->select('user_id')
+            ->where('user_id',$userId)
+            ->get('calendar')->row();
+        if(count($userExists)> 0){
+            $this->db->update('calendar',$tokkenArray);
+        }
+        else{
+            $this->db->insert('calendar',$tokkenArray);
+        }
+        return $data;
+    }
+
+    public function GetRefreshedAccessToken($client_id, $refresh_token, $client_secret) {
+        $url_token = 'https://www.googleapis.com/oauth2/v4/token';
+        $curlPost = 'client_id=' . $client_id . '&client_secret=' . $client_secret . '&refresh_token='. $refresh_token . '&grant_type=refresh_token';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url_token);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
+        $data = json_decode(curl_exec($ch), true);	//print_r($data);
+        $http_code = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+        if($http_code != 200)
+            throw new Exception('Error : Failed to refresh access token');
+
+        $userId=1;
+        $tokkenArray=array(
+            'access_token'=>$data['access_token'],
+            'access_expiry_time'=>date('Y-m-d H:i:s'),
+        );
+        $this->db->where('user_id',$userId)
+            ->update('calendar',$tokkenArray);
+        return $data;
+    }
+
+    public function getAccessTokenDetails(){
+//        $id=$this->session->user_id;
+        $id=1;
+        return $this->db->where('user_id',$id)
+        ->get('calendar')->row();
+    }
+
+    public function GetUserCalendarTimezone($access_token)
+    {
+        $url_settings = 'https://www.googleapis.com/calendar/v3/users/me/settings/timezone';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url_settings);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $access_token));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        $data = json_decode(curl_exec($ch), true); //echo '<pre>';print_r($data);echo '</pre>';
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if ($http_code != 200)
+            throw new Exception('Error : Failed to get timezone');
+
+        return $data['value'];
+    }
+
+    public function getCalendarEventById($calendarId = 'primary',$eventId)
+    {
+        $url_events = 'https://www.googleapis.com/calendar/v3/calendars/' . $calendarId . '/events/'.$eventId;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url_events);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $this->session->access_token, 'Content-Type: application/json'));
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($curlPost));
+        $data = json_decode(curl_exec($ch), true);
+        return $data;
+    }
+
+    public function GetCalendarsList($access_token)
+    {
+        $url_parameters = array();
+
+        $url_parameters['fields'] = 'items(id,summary,timeZone)';
+        $url_parameters['minAccessRole'] = 'owner';
+
+        $url_calendars = 'https://www.googleapis.com/calendar/v3/users/me/calendarList?' . http_build_query($url_parameters);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url_calendars);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $access_token));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        $data = json_decode(curl_exec($ch), true); //echo '<pre>';print_r($data);echo '</pre>';
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if ($http_code != 200)
+            throw new Exception('Error : Failed to get calendars list');
+
+        return $data['items'];
+    }
+
+    public function CreateCalendarEvent($calendar_id, $summary, $all_day, $event_time, $event_timezone, $access_token) {
+        $url_events = 'https://www.googleapis.com/calendar/v3/calendars/' . $calendar_id . '/events';
+
+//        $curlPost['description']=$description;
+        $curlPost = array('summary' => $summary);
+        if($all_day == 1) {
+            $curlPost['start'] = array('date' => $event_time['event_date']);
+            $curlPost['end'] = array('date' => $event_time['event_date']);
+        }
+        else {
+            $curlPost['start'] = array('dateTime' => $event_time['start_time'], 'timeZone' => $event_timezone);
+            $curlPost['end'] = array('dateTime' => $event_time['end_time'], 'timeZone' => $event_timezone);
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url_events);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '. $access_token, 'Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($curlPost));
+        $data = json_decode(curl_exec($ch), true);
+        $http_code = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+        if($http_code != 200)
+            throw new Exception('Error : Failed to create event');
+
+        return $data['id'];
+    }
+
+    public function getCalendarEvents($calendarId = 'primary')
+    {
+        $timeMin = date("c", strtotime(date('Y-m-01 ') . ' 00:00:00'));
+        $timeMax = date("c", strtotime(date('Y-m-t ') . ' 23:59:59'));
+        $optParams = array(
+//            'maxResults'   => $maxResults,
+            'orderBy' => 'startTime',
+            'singleEvents' => true,
+            'timeMin' => $timeMin,
+            'timeMax' => $timeMax,
+            'timeZone' => 'Asia/Kolkata',
+
+        );
+        $url_events = 'https://www.googleapis.com/calendar/v3/calendars/' . $calendarId . '/events';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url_events);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $this->session->access_token, 'Content-Type: application/json'));
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($curlPost));
+        $data = json_decode(curl_exec($ch), true);
+//        echo "<pre>";
+//        print_r($data);die;
+        return $data;
+    }
+
+    public function UpdateCalendarEvent($event_id, $calendar_id, $summary, $all_day, $event_time, $event_timezone, $access_token)
+    {
+        $url_events = 'https://www.googleapis.com/calendar/v3/calendars/' . $calendar_id . '/events/' . $event_id;
+        $curlPost = array('summary' => $summary);
+        if($all_day == 1) {
+            $curlPost['start'] = array('date' => $event_time['event_date']);
+            $curlPost['end'] = array('date' => $event_time['event_date']);
+        }
+        else {
+            $curlPost['start'] = array('dateTime' => $event_time['start_time'], 'timeZone' => $event_timezone);
+            $curlPost['end'] = array('dateTime' => $event_time['end_time'], 'timeZone' => $event_timezone);
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url_events);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '. $access_token, 'Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($curlPost));
+        $data = json_decode(curl_exec($ch), true);
+
+        $http_code = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+        if($http_code != 200)
+            throw new Exception('Error : Failed to update event');
+        else
+            return true;
+    }
+
+    //drag update
+    public function UpdateCalendaronDrag($event_id, $calendar_id, $summary, $all_day, $event_time, $event_timezone, $access_token)
+    {
+        $url_events = 'https://www.googleapis.com/calendar/v3/calendars/' . $calendar_id . '/events/' . $event_id;
+        $curlPost = array('summary' => $summary);
+        if($all_day == 1) {
+            $curlPost['start'] = array('date' => $event_time['event_date']);
+            $curlPost['end'] = array('date' => $event_time['event_date']);
+        }
+        else {
+            $curlPost['start'] = array('dateTime' => $event_time['start_time'], 'timeZone' => $event_timezone);
+            $curlPost['end'] = array('dateTime' => $event_time['end_time'], 'timeZone' => $event_timezone);
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url_events);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '. $access_token, 'Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($curlPost));
+        $data = json_decode(curl_exec($ch), true);
+        $http_code = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+        if($http_code != 200)
+            throw new Exception('Error : Failed to update event');
+        else
+            return true;
+    }
+
+    public function DeleteCalendarEvent($event_id, $calendar_id, $access_token) {
+        $url_events = 'https://www.googleapis.com/calendar/v3/calendars/' . $calendar_id . '/events/' . $event_id;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url_events);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '. $access_token, 'Content-Type: application/json'));
+        $data = json_decode(curl_exec($ch), true);
+
+        $http_code = curl_getinfo($ch,CURLINFO_HTTP_CODE);
+        if($http_code != 204)
+            throw new Exception('Error : Failed to delete event');
+        else
+            return true;
+    }
+
+}
+
+?>
---
+++ 09-04-19/Welcome.php
@@ -0,0 +1,151 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Modelwelcome', 'model');
+        error_reporting(E_ALL);
+    }
+
+    public function index()
+    {
+//        $this->session->sess_destroy('access_token'); die;
+
+        // if user visits for the first time
+        if ($this->input->get('code')) {
+            $code = $this->input->get('code');
+            $tokenData = $this->model->GetAccessToken(CLIENT_ID, CLIENT_REDIRECT_URL, CLIENT_SECRET, $code);
+            $this->session->set_userdata('access_token', $tokenData['access_token']);
+            redirect('welcome/getEvents');
+        }
+        if ($this->session->access_token) {
+            redirect('welcome/getEvents');
+        } else {
+            $this->load->view('calendar');
+        }
+    }
+
+    public function getEvents()
+    {
+        if ($this->session->access_token) {
+            $accessDetails = $this->model->getAccessTokenDetails();
+            if (!$this->checkToken($accessDetails)) {
+                $refreshedToken = $this->model->GetRefreshedAccessToken(CLIENT_ID, $accessDetails->refresh_token, CLIENT_SECRET);
+                $this->session->set_userdata('access_token', $refreshedToken['access_token']);
+            }
+            $events = $this->model->getCalendarEvents();
+            $eventsArr = array();
+            $arrCounter = 0;
+            foreach ($events['items'] as $row) {
+                $eventsArr[$arrCounter]['id'] = $row['id'];
+                $eventsArr[$arrCounter]['title'] = $row['summary'];
+                $eventsArr[$arrCounter]['fullStartDateTime'] = $row['start']['dateTime'];
+                $eventsArr[$arrCounter]['fullEndDateTime'] = $row['end']['dateTime'];
+                $eventsArr[$arrCounter]['start'] = substr($row['start']['dateTime'], 0, 10);
+                $eventsArr[$arrCounter]['end'] = substr($row['end']['dateTime'], 0, 10);
+                $arrCounter++;
+            }
+            $data['calendarEvents'] = $eventsArr;
+            $this->load->view('list', $data);
+        } else {
+            redirect('welcome');
+        }
+    }
+
+    public function addEvent()
+    {
+
+        $access_token = $this->session->access_token;
+        $calendarId = 'primary';
+        $eventTitle = $this->input->post('eventTitle');
+        $fullDayEvent = 0;
+        $timezone = $this->model->GetUserCalendarTimezone($access_token);
+
+        $startDate = str_replace(' ', 'T', $this->input->post('startDate'));
+        $startDate .= ':00';
+        $endDate = str_replace(' ', 'T', $this->input->post('endDate'));
+        $endDate .= ':00';
+        $eventTime = array('start_time' => $startDate, 'end_time' => $endDate);
+        $id=$this->model->CreateCalendarEvent($calendarId, $eventTitle, $fullDayEvent, $eventTime, $timezone, $access_token);
+        $data['response'] = "Added";
+        $data['eventId']=$id;
+        echo json_encode($data);
+    }
+
+    public function updateCalendarEvent()
+    {
+        $access_token = $this->session->access_token;
+        $timezone = $this->model->GetUserCalendarTimezone($this->session->access_token);
+        $calendarId = 'primary';
+        $eventId = $this->input->post('eventId');
+        $fullDayEvent = 0;
+        // get event details by Event Id
+        $events = $this->model->getCalendarEventById('primary', $eventId);
+//        echo "<pre>";
+//        print_r($events);die;
+        $eventTitle = $events['summary'];
+
+        $eventType = $this->input->post('eventType');
+        if ($eventType == "resize") {
+            $startDate = $this->input->post('startDate');
+            $endDate = $this->input->post('endDate');
+            $startmodified = $this->getOnlyTime($events['start']['dateTime']);
+            $endmodified = $this->getOnlyTime($events['end']['dateTime']);
+            $fullStartDate = $startDate . "T" . $startmodified;
+            $fullEndDate = $endDate . "T" . $endmodified;
+            $eventTime = array('start_time' => $fullStartDate, 'end_time' => $fullEndDate);
+        } else if ($eventType == "drop") {
+            $movedDate = $this->input->post('movedDate');
+            $startmodified = $this->getOnlyTime($events['start']['dateTime']);
+            $endmodified = $this->getOnlyTime($events['end']['dateTime']);
+            $fullStartDate = $movedDate . "T" . $startmodified;
+            $fullEndDate = $movedDate . "T" . $endmodified;
+            $eventTime = array('start_time' => $fullStartDate, 'end_time' => $fullEndDate);
+        } else if ($eventType == "eventUpdate") {
+            $eventTitle = $this->input->post('eventTitle');
+            $startDate = str_replace(' ', 'T', $this->input->post('startDate'));
+            $startDate .= ':00';
+            $endDate = str_replace(' ', 'T', $this->input->post('endDate'));
+            $endDate .= ':00';
+            $eventTime = array('start_time' => $startDate, 'end_time' => $endDate);
+        }
+        //Update Event
+        $this->model->UpdateCalendarEvent($eventId, $calendarId, $eventTitle, $fullDayEvent, $eventTime, $timezone, $access_token);
+        $data['response'] = "updated";
+        echo json_encode($data);
+    }
+
+    public function deleteEvent()
+    {
+        $eventId = $this->input->post('eventId');
+//        $eventId='1rivdnnar63gu8hni7mvkbstli';
+        $result = $this->model->DeleteCalendarEvent($eventId, 'primary', $this->session->access_token);
+        if ($result) {
+            $data['response'] = "deleted";
+            echo json_encode($data);
+        }
+    }
+
+    public function checkToken($accessDetails)
+    {
+        $datetime1 = new DateTime($accessDetails->access_expiry_time);
+        $datetime2 = new DateTime('now');
+        $interval = $datetime1->diff($datetime2);
+        if ($interval->format('%i') > 58 || $interval->format('%h') > 0 || $interval->format('%a') > 0 || $interval->format('%m') > 0 || $interval->format('%y') > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function getOnlyTime($time)
+    {
+        $afterT = explode('T', $time);
+        $startTime = explode('+', $afterT[1]);
+        return $startTime['0'];
+    }
+
+}
