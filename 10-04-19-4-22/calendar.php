86d3df7a12d96208caed750490b65be6b8b3119b
Rishi Ezhava <rishiezhava@gmail.com>
2019-04-10 16:34:25 +0530

    finale

---
+++ 10-04-19-4-22/calendar.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <title> G C</title>
+
+    <style type="text/css">
+
+        #logo {
+            text-align: center;
+            width: 200px;
+            display: block;
+            margin: 100px auto;
+            border: 2px solid #2980b9;
+            padding: 10px;
+            background: none;
+            color: #2980b9;
+            cursor: pointer;
+            text-decoration: none;
+        }
+
+    </style>
+
+    <?php
+    $login_url = 'https://accounts.google.com/o/oauth2/auth?scope=' . urlencode('https://www.googleapis.com/auth/calendar') . '&redirect_uri=' . urlencode(CLIENT_REDIRECT_URL) . '&response_type=code&client_id=' . CLIENT_ID . '&access_type=offline';
+
+    ?>
+</head>
+<body>
+<a id="logo" href="<?= $login_url ?>">Login with Google</a>
+</body>
+</html>
